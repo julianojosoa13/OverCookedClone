13ce6932c3605f42ec31e0b628f74cd39db2075e
julianojosoa13 <josoajuliano@gmail.com>
2024-12-04 21:53:53 +0300

    Basic character controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private void Update() {
-        
+    private void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        transform.position += moveDir * Time.deltaTime * 10;
     }
 }

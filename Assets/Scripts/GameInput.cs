c46aff4334587086d82252e49630b189a501d223
julianojosoa13 <josoajuliano@gmail.com>
2025-03-18 19:12:19 +0300

    Refactor: Using the new Input System

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -4,27 +4,18 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
     // Start is called before the first frame update
     public Vector2 GetMovementVector()
     {
-        Vector2 inputVector = Vector2.zero;
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
 
         inputVector = inputVector.normalized;
         return inputVector;

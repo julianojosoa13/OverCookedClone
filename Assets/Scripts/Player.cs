2c15b69e9cd7c8d8ca5261a930fb5e1a10c03d4f
julianojosoa13 <josoajuliano@gmail.com>
2025-04-27 16:54:02 +0300

    Normalizing diagonal movement direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,13 +25,13 @@ public class Player : MonoBehaviour
 
        
         if(!canMove) {
-            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance );
 
             if(canMove) {
                 moveDir = moveDirX;
             } else {
-                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance );
                 if(canMove) {
                     moveDir = moveDirZ;

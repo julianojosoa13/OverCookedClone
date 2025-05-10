9ec83ab351a29daeead240489d6ddfee0147f2e4
julianojosoa13 <josoajuliano@gmail.com>
2025-05-10 20:03:46 +0300

    Player pickup and drop object on Clear Counter  mechanics

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,6 +8,16 @@ public class ClearCounter : BaseCounter
    
 
     public override void Interact(Player player) {
-        
+        if(!HasKitchenObject()) {
+            if(player.HasKitchenObject()) {
+                KitchenObject kitchenObject = player.GetKitchenObject();
+                kitchenObject.SetKitchenObjectParent(this);
+            }
+        } else {
+            if(!player.HasKitchenObject()) {
+                KitchenObject kitchenObject = GetKitchenObject();
+                kitchenObject.SetKitchenObjectParent(player);
+            }
+        }
     }
 }

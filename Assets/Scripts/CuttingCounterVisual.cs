900718f80a95aa8c477aa92f88a43687fa4d648e
julianojosoa13 <josoajuliano@gmail.com>
2025-05-11 13:09:58 +0300

    Cutting animation visual

---
+++ Assets/Scripts/CuttingCounterVisual.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingCounterVisual : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private const string CUT = "Cut";
+
+    [SerializeField] private CuttingCounter cuttingCounter;
+
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+    }
+
+    private void CuttingCounter_OnProgressChanged(object sender , CuttingCounter.OnProgressChangedEventArgs e) {
+        if(e.progressNormalized > 0) {
+            animator.SetTrigger(CUT);
+        }
+    }
+}
